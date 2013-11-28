f31ef864b83f3f4b41a685bf4a9e5568dd55a2cd
Marcos Garcia de Lima <marckfree@gmail.com>
2013-11-28 08:13:12 -0200

    Adiciona arquivos header.php e footer.php

---
+++ wordpress/wp-content/themes/revista-maua-e-regiao/footer.php
@@ -0,0 +1,41 @@
+     <div class="rodape">
+        <div class="conteudos">
+          <div class="menu">
+            <ul>
+              <li>
+                <div class="borda-link-menu"></div>
+                <a href="#todas-as-edicoes">Todas as edições</a></li>
+              <li>
+                <div class="borda-link-menu"></div>
+                <a href="#gabaritos">Gabaritos</a></li>
+              <li>
+                <div class="borda-link-menu"></div>
+                <a href="#sobre-nos">Sobre nós</a></li>
+              <li>
+                <div class="borda-link-menu"></div>
+                <a href="#anuncie">Anuncie</a></li>
+              <li>
+                <div class="borda-link-menu"></div>
+                <a href="#fale-conosco">Fale conosco</a></li>
+            </ul>
+          </div>
+
+          <div class="logotipo">
+            <a href="http://grupomauaeregiao.com.br" target="_blank">
+              <div class="logotipo-grupo-maua-e-regiao" title="Grupo Mauá e Região"></div>
+            </a>
+          </div>
+
+          <div class="direitos-autorais">
+            <p>&copy; 2013 Grupo Mauá e Região &#8212; Todos os direitos reservados</p>
+          </div>
+        </div>
+      </div>
+      <!-- # Rodapé -->
+    </div>
+    <!-- # Layout -->
+
+    <?php wp_footer(); ?>
+    <script src="scripts/scripts.js"></script>
+  </body>
+</html>
