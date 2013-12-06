02ee55ecb18d1f711eaf204079678af063b6779c
Marcos Garcia de Lima <marckfree@gmail.com>
2013-12-06 08:23:05 -0200

    Implementa a página 'Sobre nós'
    * Frontend apenas

---
+++ wordpress/wp-content/themes/revista-maua-e-regiao/sobre-nos.php
@@ -0,0 +1,64 @@
+<div class="conteudo">
+  <div class="sobre-nos">
+    <div class="cabecalho-sobre-nos">
+      <h1>Sobre nós</h1>
+    </div>
+
+    <div class="texto-sobre-nos">
+
+      <div class="cabecalho-texto-sobre-nos">
+        <h2>A Revista Mauá e Região é hoje, um dos nomes mais importantes na Região</h2>
+      </div>
+
+      <div class="corpo-texto-sobre-nos">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, cupiditate, ipsum, non, quis ratione velit inventore facilis expedita assumenda sint modi accusamus amet at quos adipisci repellendus reiciendis est excepturi. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, nulla, laboriosam, quaerat, ducimus velit saepe nihil delectus expedita iusto distinctio numquam impedit sint magnam id reprehenderit est magni accusantium voluptas!
+        </p>
+        <p>
+          Ipsum dolor sit amet, consectetur adipisicing elit. Voluptate, mollitia, quas, voluptates quia soluta laborum vero consectetur aperiam ratione cupiditate alias saepe quo sit. Distinctio, illum dicta repellendus harum quis.
+        </p>
+        <p>
+          Sit amet, consectetur adipisicing elit. Ab, vitae, natus atque aspernatur provident placeat consequuntur aut reprehenderit ducimus eligendi aliquam animi pariatur optio. Ad temporibus autem veniam velit at. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, ea laboriosam consectetur recusandae?
+        </p>
+        <p>
+          Amet, consectetur adipisicing elit. Voluptas, veniam, dolore officia rerum quidem facilis quae neque beatae nesciunt consequuntur nemo ducimus perferendis voluptatibus ab dolorem temporibus et fugiat deserunt. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis, quod, et laborum impedit.
+        </p>
+      </div>
+    </div>
+
+    <div class="icones-destaques">
+      <div class="icones">
+
+        <div class="icone">
+          <div class="imagem"></div>
+          <div class="texto-icone-destaque">
+            <span>11 mil revistas impressas</span>
+          </div>
+        </div>
+
+        <div class="icone">
+          <div class="imagem"></div>
+          <div class="texto-icone-destaque">
+            <span>Distribuído em 3 cidades</span>
+          </div>
+        </div>
+
+        <div class="icone">
+          <div class="imagem"></div>
+          <div class="texto-icone-destaque">
+            <span>Divulgação em redes sociais</span>
+          </div>
+        </div>
+
+        <div class="icone">
+          <div class="imagem"></div>
+          <div class="texto-icone-destaque">
+            <span>Conteúdo para mobile</span>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+</div>
