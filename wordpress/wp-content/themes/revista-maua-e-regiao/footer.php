eac5ff6f75bfdac1eb0d50bd87d2979909e5fdfa
marcker <marckfree@gmail.com>
2014-01-28 08:24:23 -0200

    Adiciona código para tracking (Google)

--- wordpress/wp-content/themes/revista-maua-e-regiao/footer.php
+++ wordpress/wp-content/themes/revista-maua-e-regiao/footer.php
@@ -32,5 +32,19 @@
       <script src="<?php bloginfo('template_url'); ?>/scripts/google.maps.js"></script>
     <?php endif; ?>
     <script src="<?php bloginfo('template_url'); ?>/scripts/scripts.min.js"></script>
+    <script>
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-47556600-1']);
+      _gaq.push(['_trackPageview']);
+      (function() {
+        var ga = document.createElement('script');
+        ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol
+          ? 'https://ssl'
+          : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(ga, s);
+      })();
+    </script>
   </body>
 </html>
